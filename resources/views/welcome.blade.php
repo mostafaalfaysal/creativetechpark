e58fe438ebf7c88b90c30709e84e52ad16a7ef2e
mostafaalfaysal <mostafaalfaysal@gmail.com>
2024-01-11 17:46:56 +0600

    Added

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+
+    <!-- Tailwind Css CDN -->
+    <script src="https://cdn.tailwindcss.com"></script>
+
+    <!-- DaisyUI CDN  -->
+    <link
+      href="https://cdn.jsdelivr.net/npm/daisyui@4.6.0/dist/full.min.css"
+      rel="stylesheet"
+      type="text/css"
+    />
+    <script src="https://cdn.tailwindcss.com"></script>
+  </head>
+  <body class="p-5">
+    <!-- heading  -->
+    <section class="my-5">
+      <h1
+        class="text-center text-4xl font-bold bg-blue-500 rounded-md p-5 text-white">
+        From 16:00 TO 17:00
+      </h1>
+    </section>
+
+    <!-- content  -->
+    <section class="grid grid-cols-4 gap-4">
+      <!-- left Side item -->
+      <div class="bg-gray-100 rounded-md text-justify grid content-center px-5">
+        <h1 class="text-2xl font-bold text-stack">Mostafa Al Faysal</h1>
+        <p>
+        A Back-End Web Developer passionate about creating dynamic
+applications and experiences on the web. Proficient in PHP and the
+Laravel framework, I am passionate about coding and solving problems
+through code. I am excited to work alongside other amazing
+programmers and continue to learn and grow in my field.
+        </p>
+      </div>
+
+      <!-- right side item  -->
+      <div class="bg-gray-100 col-span-3">
+        <div class="grid grid-cols-3 gap-3">
+          <!-- 1st -->
+          <div class="card w-auto bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+
+          <!-- 2nd -->
+
+          <div class="card w-auto bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+
+          <!-- 3rd -->
+          <div class="card w-auto bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              
+            </div>
+          </div>
+
+          <!-- 4th -->
+          <div class="card w-auto bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+
+          <!-- 5th -->
+          <div class="card w-auto bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+
+          <!-- 6th -->
+          <div class="card w-auto bg-base-100 shadow-xl">
+            <figure>
+              <img
+                src="https://daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div class="card-body">
+              <h2 class="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+            </div>
+          </div>
+
+          <!-- last content  -->
+          <div class="col-span-3 bg-blue-500 rounded-md ">
+            <a
+              class="text-white font-bold text-3xl flex justify-center p-5 rounded-md"
+              href="https://github.com/mostafaalfaysal"
+              target="_blank"
+              rel="noopener noreferrer"
+              >Github Link</a
+            >
+          </div>
+        </div>
+      </div>
+    </section>
+  </body>
+</html>
